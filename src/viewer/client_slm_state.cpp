090ad16cfc73423575b77b111f6223078795d83b
Sam Potter <sfp@hush.ai>
2016-10-22 11:09:51 -0400

    fixed tabs

--- src/viewer/client_slm_state.cpp
+++ src/viewer/client_slm_state.cpp
@@ -68,37 +68,37 @@ client_slm_state::get_target_points() const
 void
 client_slm_state::recompute_phase_mask()
 {
-	recomputed_phase_mask_ = std::async(
-		std::launch::async,
-		[this] () {
-			recompute_target();
-			frame phase_mask {slm_width_, slm_height_};
-			phase_retrieval::compute_phase_mask(
-				&source_[0],
-				&target_[0],
-				img_width_,
-				img_height_,
-				slm_width_,
-				slm_height_,
-				gs_iter_count_,
-				phase_mask);
-			phase_retrieval::apply_axicon_phase_mask(
-				target_point::screen_axicon_radius,
-				phase_mask);
-			return phase_mask;
-		});
+    recomputed_phase_mask_ = std::async(
+        std::launch::async,
+        [this] () {
+            recompute_target();
+            frame phase_mask {slm_width_, slm_height_};
+            phase_retrieval::compute_phase_mask(
+                &source_[0],
+                &target_[0],
+                img_width_,
+                img_height_,
+                slm_width_,
+                slm_height_,
+                gs_iter_count_,
+                phase_mask);
+            phase_retrieval::apply_axicon_phase_mask(
+                target_point::screen_axicon_radius,
+                phase_mask);
+            return phase_mask;
+        });
 }
 
 bool
 client_slm_state::phase_mask_recomputed() const
 {
-	return recomputed_phase_mask_.valid() && is_ready(recomputed_phase_mask_);
+    return recomputed_phase_mask_.valid() && is_ready(recomputed_phase_mask_);
 }
 
 frame
 client_slm_state::get_recomputed_phase_mask()
 {
-	return recomputed_phase_mask_.get();
+    return recomputed_phase_mask_.get();
 }
 
 void
