c0309218881b77b56365b14315ecb0fddb9ec9f5
Sam Potter <sfp@hush.ai>
2017-02-11 21:11:29 -0500

    added start of Lua wrapper

---
+++ src/viewer/lua.hpp
@@ -0,0 +1,29 @@
+#ifndef __LUA_HPP__
+#define __LUA_HPP__
+
+#include <string>
+
+extern "C" {
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+}
+
+namespace lua {
+	struct state {
+		state();
+		~state();
+		bool do_file(std::string const & path) const;
+		int get_field(int index, std::string const & key) const;
+		int get_global(std::string const & name) const;
+		void open_libs() const;
+		void pop(int n) const;
+		int to_integer(int index) const;
+		double to_number(int index) const;
+		std::string to_string(int index) const;
+	private:
+		lua_State * lua_state_;
+	};
+}
+
+#endif // __LUA_HPP__
