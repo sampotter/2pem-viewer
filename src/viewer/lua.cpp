4fdf1f3d97fff5a0641dd908c90cf1c6bd4ce732
Sam Potter <sfp@hush.ai>
2017-02-11 21:14:44 -0500

    fixed spacing

--- src/viewer/lua.cpp
+++ src/viewer/lua.cpp
@@ -3,62 +3,66 @@
 #include <stdexcept>
 
 lua::state::state():
-	lua_state_ {luaL_newstate()}
+    lua_state_ {luaL_newstate()}
 {
-	if (lua_state_ == nullptr) {
-		throw std::runtime_error("Memory allocation error");
-	}
+    if (lua_state_ == nullptr) {
+        throw std::runtime_error("Memory allocation error");
+    }
 }
 
 lua::state::~state()
 {
-	lua_close(lua_state_);
+    lua_close(lua_state_);
 }
 
 bool
 lua::state::do_file(std::string const & path) const
 {
-	return luaL_dofile(lua_state_, path.c_str());
+    return luaL_dofile(lua_state_, path.c_str());
 }
 
 int
 lua::state::get_field(int index, std::string const & key) const
 {
-	return lua_getfield(lua_state_, index, key.c_str());
+    return lua_getfield(lua_state_, index, key.c_str());
 }
 
 int
 lua::state::get_global(std::string const & name) const
 {
-	return lua_getglobal(lua_state_, name.c_str());
+    return lua_getglobal(lua_state_, name.c_str());
 }
 
 void
 lua::state::open_libs() const
 {
-	luaL_openlibs(lua_state_);
+    luaL_openlibs(lua_state_);
 }
 
 void
 lua::state::pop(int n) const
 {
-	lua_pop(lua_state_, n);
+    lua_pop(lua_state_, n);
 }
 
 int
 lua::state::to_integer(int index) const
 {
-	return lua_tointeger(lua_state_, index);
+    return lua_tointeger(lua_state_, index);
 }
 
 double
 lua::state::to_number(int index) const
 {
-	return lua_tonumber(lua_state_, index);
+    return lua_tonumber(lua_state_, index);
 }
 
 std::string
 lua::state::to_string(int index) const
 {
-	return lua_tostring(lua_state_, index);
+    return lua_tostring(lua_state_, index);
 }
+
+// Local Variables:
+// indent-tabs-mode: nil
+// End:
--- src/viewer/lua.hpp
+++ src/viewer/lua.hpp
@@ -10,20 +10,24 @@ extern "C" {
 }
 
 namespace lua {
-	struct state {
-		state();
-		~state();
-		bool do_file(std::string const & path) const;
-		int get_field(int index, std::string const & key) const;
-		int get_global(std::string const & name) const;
-		void open_libs() const;
-		void pop(int n) const;
-		int to_integer(int index) const;
-		double to_number(int index) const;
-		std::string to_string(int index) const;
-	private:
-		lua_State * lua_state_;
-	};
+    struct state {
+        state();
+        ~state();
+        bool do_file(std::string const & path) const;
+        int get_field(int index, std::string const & key) const;
+        int get_global(std::string const & name) const;
+        void open_libs() const;
+        void pop(int n) const;
+        int to_integer(int index) const;
+        double to_number(int index) const;
+        std::string to_string(int index) const;
+    private:
+        lua_State * lua_state_;
+    };
 }
 
 #endif // __LUA_HPP__
+
+// Local Variables:
+// indent-tabs-mode: nil
+// End:
